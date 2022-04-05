9dc93c0fcaf32b221a0017c0d6183ebb12b07610
Hasan <Hasanaldhahi3@gmail.com>
2022-04-06 00:55:38 +0300

    not ready yet

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution here!
+
+function cats() {
+
+    cats = [];
+
+    cats.push('Milo', 'Otis', 'Garfield');
+
+}
+
+
+cats = [];
+
+cats.push('Milo', 'Otis', 'Garfield');
+
+
+cats.push('Ralph');
+
+cats.shift('Bob');
